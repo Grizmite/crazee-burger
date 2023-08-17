f4a7b4e2741103a903dec418f180dbe8ae122be1
Grizmite <chabertjb@hotmail.com>
2023-08-17 17:15:01 +0200

    git commit -m "first commit"

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Jean-Baptiste Chabert</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
