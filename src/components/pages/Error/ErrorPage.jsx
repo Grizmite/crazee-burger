1129fcf3a2bd672a326702cef86088c5e728adf3
Grizmite <chabertjb@hotmail.com>
2023-08-20 16:40:07 +0200

    F02: > add redirection 01cceef ✨ F02: APP > install errorPage

--- src/components/pages/Error/ErrorPage.jsx
+++ src/components/pages/Error/ErrorPage.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
   return (
     <div>
-      ErrorPage
+      <h1>Error Page</h1>
+      <Link to="/">
+        <button>Return</button>
+      </Link>
     </div>
   )
 }
