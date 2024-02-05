8fed28cccfb2e6ba838846a786350236b4439171
Grizmite <chabertjb@hotmail.com>
2024-02-05 22:01:21 +0100

    🚧 F05: ORDER > dd product infos

--- src/components/pages/Order/Main/Menu.jsx
+++ src/components/pages/Order/Main/Menu.jsx
@@ -15,26 +15,48 @@ export default function Menu() {
 
     <MenuStyled>
         {menu.map((produit) => {
-           return <div className='produit'>{produit.title}</div>
+           return (
+            <div key={produit.id} className='produit'>
+            <div  className='image'>
+                <img src={produit.imageSource} alt={produit.title} />
+            </div>
+            <div className='info-text'>
+           <div className='title'>{produit.title}</div>
+           <div className='price'>{produit.price}</div>
+           <button>X</button>
+           </div>
+           </div>
+           )
+           
         })}
-    </MenuStyled>
+    </MenuStyled> 
   )
 }
 
 
 const MenuStyled = styled.div`
 
-    background: purple;
+    background: #F5F5F7;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-row-gap: 60px;
-    padding: 50px 50px 50 px;
+    padding: 50px 50px 150 px;
     justify-items: center;
+    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   
     .produit{
         background: red;
         width: 240px;
         height: 330px;
+    }
+
+    .image{
+        width: 100px;
+        height: auto;
+        img{
+            width: 100%;
+            height: 100%;
+        }
     }
   
   `;
