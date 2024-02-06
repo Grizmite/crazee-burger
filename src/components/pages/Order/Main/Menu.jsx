55feee986ce93c6be70352dbdadaf8b175a0d990
Grizmite <chabertjb@hotmail.com>
2024-02-06 14:04:02 +0100

    ♻️ F05: Add menu

--- src/components/pages/Order/Main/Menu.jsx
+++ src/components/pages/Order/Main/Menu.jsx
@@ -41,27 +41,4 @@ const MenuStyled = styled.div`
     justify-items: center;
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   
-    .produit{
-        background: red;
-        width: 240px;
-        height: 330px;
-    }
-
-    .image{
-
-        width: 100px;
-        height: auto;
-        img{
-            width: 100%;
-            height: 100%;
-            border: 1px solid blue;
-
-        }
-        
-    }
-
-    .description{
-            border: 1px solid blue;
-        }
-  
   `;
